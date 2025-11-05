8bd9461e89599c497fc2cfecbc33cdf95061f4e5
Ogongus <whjtan4357@gmail.com>
2025-11-04 23:15:29 -0600

    Add int main

--- main.cpp
+++ main.cpp
@@ -3,3 +3,12 @@
 #include <sstream>
 #include "calculator.h"
 
+int main(int argc, char* argv[]) {
+    std::string filename = (argc > 1) ? argc[1] : "input.txt";
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {
+        std::cerr << "Error: Cannot open file " << filename << std::endl;
+        return 1;
+    }
+}
