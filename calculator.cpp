d072571c4ce6ef8608f32587eb877dd15ccd2105
Ogongus <whjtan4357@gmail.com>
2025-11-04 22:47:47 -0600

    add removeLeadingZeros

--- calculator.cpp
+++ calculator.cpp
@@ -9,3 +9,31 @@ std::string trimLeadingZeros(const std::string& s) {
     return s.substr(start);
 }
 
+std::string removeLeadingZeros(const std::string& s) {
+    if (s.empty() || s == "+" || s == "-") return s;
+    std::string num = s;
+    char sign = '+';
+    size_t i = 0;
+
+    if (num[0] == '+' || num[0] == '-') {
+        sign = num[0];
+        num = num.substr(1);
+    }
+
+    size_t dot = num.find('.');
+    std::string intPart = (dot == std::string::npos) ? num : num.substr(0, dot);
+    std::string fracPart = (dot == std::string::npos) ? "" : num.substr(dot + 1);
+
+    intPart = trimLeadingZeros(intPart);
+    if (intPart.empty()) intPart = "0";
+
+    std::string result = std::string(1, sign) + intPart;
+    if (!fracPart.empty()) {
+        result += "." + fracPart;
+    }
+    if (sign == '+' && result[0] == '+') {
+        result = result.substr(1);
+    }
+    return result;
+}
+
